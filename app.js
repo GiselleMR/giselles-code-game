9a4022bb959300e9e932d73bbaea99d642efb1cd
GiselleMR <giselle.rubin1@gmail.com>
2023-02-07 17:13:52 -0500

    fix js layout

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// declaring our global variables
+//Variables
 let COLOR = "";
 let CURRENT_CIRCLE = 0;
 const COLOR_ARRAY = ["pink", "teal", "blue", "green"];
 const WINNING_COLOR_ARRAY = shuffleArray(COLOR_ARRAY);
 let current_color_array = [];
 
-// NEW CODE
+
 const colorMap = {
   "rgb(252, 3, 182)": "pink",
   "rgb(3, 252, 11)": "green",
@@ -15,7 +15,7 @@ const colorMap = {
 
 console.log("WINNING_COLOR_ARRAY", WINNING_COLOR_ARRAY);
 
-// Grabbing the necessary html elements from the DOM
+// DOM elements
 const mastermind = document.getElementById("mastermind");
 const singleColorArray = document.querySelectorAll(".singlecolor");
 
@@ -28,10 +28,6 @@ singleColorArray.forEach(function (element) {
   element.addEventListener("click", function (evt) {
     // NEW CODE
     if (CURRENT_CIRCLE >= 24) {
-      // we don't want to try to update a circle that doesn't exist
-      // since the ids only go up to 24
-      // this is also where you would implement the losing logic;
-      // if you reach circle #24 without finding a match, you've lost the game
       alert("LOST THE GAME!");
       return;
     }
@@ -55,31 +51,16 @@ singleColorArray.forEach(function (element) {
   });
 });
 
-//All Helper functions
+//functions
 
-/**
- * Shuffles array in place.
- * @param {Array} a items An array containing the items.
- * https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
- */
 function shuffleArray(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
-  }
-  return a;
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
-/**
- * Checks to see if the first and second array are equal and in the same order
- * @param {Array} arr1
- * @param {Array} arr2
- * @returns true or false
- * check to see if winning color array and current color array are the same 
- * https://stackoverflow.com/questions/22395357/how-to-compare-two-arrays-are-equal-using-javascript
- */
+
 function isSame(arr1, arr2) {
   return (
     arr1.length === arr2.length &&
